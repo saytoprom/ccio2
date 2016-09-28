167b5f026215d1aaba1e5bc36b8f9c8579606707
chucc <chucc@chinanetcenter.com>
2016-09-28 11:01:24 +0800

    update cio.cc

--- src/cio.cc
+++ src/cio.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <node.h>
+#include <map>
 
 namespace cio {
 
@@ -10,6 +11,7 @@ using v8::Isolate;
 using v8::Local;
 using v8::Object;
 using v8::Number;
+using v8::Boolean;
 using v8::String;
 using v8::Value;
 
@@ -17,34 +19,62 @@ using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using std::map;
+
+map<string, int> typeMap;
 
 void Input(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
   string str, type;
   double num;
+  bool boolean;
   Local<String> jsType;
   if(args.Length() < 1){
     cin>>str;
     args.GetReturnValue().Set(String::NewFromUtf8(isolate, str.c_str()));
   } else {
-    jsType = Local<String>::Cast(args[1]);
-    type = string(*String::Utf8Value(jsType));
-    cout<<type<<endl;
-    if(type == "Number"){
-      cin>>num;
-      args.GetReturnValue().Set(Number::New(isolate, num));
-    } else if(type == "String"){
-      cin>>str;
-      args.GetReturnValue().Set(String::NewFromUtf8(isolate, str.c_str()));
+    jsType = Local<String>::Cast(args[0]);
+    String::Utf8Value utf8Value(jsType);
+    type = string(*utf8Value);
+    cout<<typeMap[type]<<endl;
+    switch(typeMap[type]){
+      case 0:
+        cin>>num;
+        args.GetReturnValue().Set(Number::New(isolate, num));
+        break;
+      case 1:
+        cin>>str;
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, str.c_str()));
+        break;
+      case 2:
+        cin>>boolean;
+        args.GetReturnValue().Set(Boolean::New(isolate, boolean));
+        break;
+      default:
+        break;
     }
   }
 }
 
 void Output(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
+  if(args.Length() < 1){
+    // isolate->ThrowException(Exception::TypeError(
+    //     String::NewFromUtf8(isolate, "Wrong number of arguments")));
+    cout<<endl;
+    return;
+  }
+  cout<<string(*String::Utf8Value(Local<String>::Cast(args[0])));
+  if(args.Length() > 1 && !(Local<Boolean>::Cast(args[1]))->BooleanValue())
+    return;
+  cout<<endl;
 }
 
 void init(Local<Object> exports) {
+  int typeCount = 0;
+  typeMap["Number"] = typeMap["number"] = typeCount++;
+  typeMap["String"] = typeMap["string"] = typeCount++;
+  typeMap["Boolean"] = typeMap["boolean"] = typeCount++;
   NODE_SET_METHOD(exports, "input", Input);
   NODE_SET_METHOD(exports, "output", Output);
 }
--- test.js
+++ test.js
@@ -1,9 +1,30 @@
 var cio = require('./build/Release/cio');
 
-var str = cio.input();
+// var str = cio.input();
 // console.log(str);
+// console.log(typeof str);
+
+// var num = cio.input('Number');
+// console.log(num);
+// console.log(typeof num);
+
+// var bool = cio.input('Boolean');
+// console.log(bool);
+// console.log(typeof bool);
+
+var str = cio.input('string');
 console.log(typeof str);
 
-var num = cio.input();
-// console.log(num);
-console.log(typeof num);
+var str2 = cio.input();
+console.log(typeof str2);
+
+
+// cio.output('hello');
+
+// var w = 'w'
+
+// cio.output('hello ' + w + 'orld');
+
+// cio.output('no endl...', false);
+
+// cio.output('end');
